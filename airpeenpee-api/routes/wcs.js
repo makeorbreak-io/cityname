30c00fcec8b419a637d3ac5b6e93888e8c7ad675
Hélder Pereira <blackspiider.86@gmail.com>
2017-09-08 19:20:38 +0100

    removed authentication

--- airpeenpee-api/routes/wcs.js
+++ airpeenpee-api/routes/wcs.js
@@ -3,26 +3,25 @@ module.exports = app => {
   const WCCategories = app.db.models.WCCategories;
 
   app.route('/wcs')
-    .all(app.auth.authenticate())
+    // .all(app.auth.authenticate())
     /**
-     * @api {get} /tasks List the user's tasks
-     * @apiGroup Tasks
+     * @api {get} /wcs List all WCs
+     * @apiGroup WCs
      * @apiHeader {String} Authorization Token of authenticated user
      * @apiHeaderExample {json} Header
      *    {"Authorization": "JWT xyz.abc.123.hgf"}
-     * @apiSuccess {Object[]} tasks Task's list
-     * @apiSuccess {Number} tasks.id Task id
-     * @apiSuccess {String} tasks.title Task title
-     * @apiSuccess {Boolean} tasks.done Task is done?
-     * @apiSuccess {Date} tasks.updated_at Update's date
-     * @apiSuccess {Date} tasks.created_at Register's date
-     * @apiSuccess {Number} tasks.user_id Id do usuário
+     * @apiSuccess {Object[]} WCss WCs's list
+     * @apiSuccess {Number} WCss.id WCs id
+     * @apiSuccess {String} WCss.title WCs title
+     * @apiSuccess {Boolean} WCss.done WCs is done?
+     * @apiSuccess {Date} WCss.updated_at Update's date
+     * @apiSuccess {Date} WCss.created_at Register's date
+     * @apiSuccess {Number} WCss.user_id Id do usuário
      * @apiSuccessExample {json} Success
      *    HTTP/1.1 200 OK
      *    [{
      *      "id": 1,
-     *      "title": "Study",
-     *      "done": false
+     *      "name": "Study",
      *      "updated_at": "2016-02-10T15:46:51.778Z",
      *      "created_at": "2016-02-10T15:46:51.778Z",
      *      "user_id": 1
@@ -33,25 +32,25 @@ module.exports = app => {
     .get((req, res) => {
       WCs.findAll({
         include: [WCCategories],
-        where: { user_id: req.user.id },
+        // where: { user_id: req.user.id },
       })
       .then(result => res.json(result))
       .catch(error => {
         res.status(412).json({ msg: error.message });
       });
-    })
+    });
     /**
-     * @api {post} /tasks Register a new task
-     * @apiGroup Tasks
+     * @api {post} /WCss Register a new WCs
+     * @apiGroup WCss
      * @apiHeader {String} Authorization Token of authenticated user
      * @apiHeaderExample {json} Header
      *    {"Authorization": "JWT xyz.abc.123.hgf"}
-     * @apiParam {String} title Task title
+     * @apiParam {String} title WCs title
      * @apiParamExample {json} Input
      *    {"title": "Study"}
-     * @apiSuccess {Number} id Task id
-     * @apiSuccess {String} title Task title
-     * @apiSuccess {Boolean} done=false Task is done?
+     * @apiSuccess {Number} id WCs id
+     * @apiSuccess {String} title WCs title
+     * @apiSuccess {Boolean} done=false WCs is done?
      * @apiSuccess {Date} updated_at Update's date
      * @apiSuccess {Date} created_at Register's date
      * @apiSuccess {Number} user_id User id
@@ -68,27 +67,27 @@ module.exports = app => {
      * @apiErrorExample {json} Register error
      *    HTTP/1.1 412 Precondition Failed
      */
-    .post((req, res) => {
+    /* .post((req, res) => {
       req.body.user_id = req.user.id;
       WCs.create(req.body)
         .then(result => res.json(result))
         .catch(error => {
           res.status(412).json({ msg: error.message });
         });
-    });
+    });*/
 
-  app.route('/tasks/:id')
+  app.route('/wcs/:id')
     .all(app.auth.authenticate())
     /**
-     * @api {get} /tasks/:id Get a task
-     * @apiGroup Tasks
+     * @api {get} /wcs/:id Get a WCs
+     * @apiGroup WCss
      * @apiHeader {String} Authorization Token of authenticated user
      * @apiHeaderExample {json} Header
      *    {"Authorization": "JWT xyz.abc.123.hgf"}
-     * @apiParam {id} id Task id
-     * @apiSuccess {Number} id Task id
-     * @apiSuccess {String} title Task title
-     * @apiSuccess {Boolean} done Task is done?
+     * @apiParam {id} id WCs id
+     * @apiSuccess {Number} id WCs id
+     * @apiSuccess {String} title WCs title
+     * @apiSuccess {Boolean} done WCs is done?
      * @apiSuccess {Date} updated_at Update's date
      * @apiSuccess {Date} created_at Register's date
      * @apiSuccess {Number} user_id User id
@@ -102,7 +101,7 @@ module.exports = app => {
      *      "created_at": "2016-02-10T15:46:51.778Z",
      *      "user_id": 1
      *    }
-     * @apiErrorExample {json} Task not found error
+     * @apiErrorExample {json} WCs not found error
      *    HTTP/1.1 404 Not Found
      * @apiErrorExample {json} Find error
      *    HTTP/1.1 412 Precondition Failed
@@ -124,14 +123,14 @@ module.exports = app => {
       });
     })
     /**
-     * @api {put} /tasks/:id Update a task
-     * @apiGroup Tasks
+     * @api {put} /WCss/:id Update a WCs
+     * @apiGroup WCss
      * @apiHeader {String} Authorization Token of authenticated user
      * @apiHeaderExample {json} Header
      *    {"Authorization": "JWT xyz.abc.123.hgf"}
-     * @apiParam {id} id Task id
-     * @apiParam {String} title Task title
-     * @apiParam {Boolean} done Task is done?
+     * @apiParam {id} id WCs id
+     * @apiParam {String} title WCs title
+     * @apiParam {Boolean} done WCs is done?
      * @apiParamExample {json} Input
      *    {
      *      "title": "Work",
@@ -153,12 +152,12 @@ module.exports = app => {
       });
     })
     /**
-     * @api {delete} /tasks/:id Remove a task
-     * @apiGroup Tasks
+     * @api {delete} /WCss/:id Remove a WCs
+     * @apiGroup WCss
      * @apiHeader {String} Authorization Token of authenticated user
      * @apiHeaderExample {json} Header
      *    {"Authorization": "JWT xyz.abc.123.hgf"}
-     * @apiParam {id} id Task id
+     * @apiParam {id} id WCs id
      * @apiSuccessExample {json} Success
      *    HTTP/1.1 204 No Content
      * @apiErrorExample {json} Delete error

89daf86f284b8942045858dc74739f574d2d7016
PedroMiguelFMoreira <1140781@isep.ipp.pt>
2017-09-09 17:23:32 +0100

    Removed real frontend.

--- airpeenpee-real-frontend/src/app/core/config/config.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from "@angular/core";
-import { IConfig } from "./iconfig";
-
-/* Configuration export */
-export let CONFIG = new InjectionToken('app.configuration');
-
-export const Config: IConfig = {
-  applicationName: "AirPeeNPee",
-};

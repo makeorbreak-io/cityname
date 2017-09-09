7959f9501653845af823762e98a4d2594f4cce3f
PedroMiguelFMoreira <1140781@isep.ipp.pt>
2017-09-09 15:36:20 +0100

    New build of angular with Boostrap and Core MOdule for configurations.

--- airpeenpee-real-frontend/src/app/app.module.ts
+++ airpeenpee-real-frontend/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+/* Root Component / Landing Page */
 import { AppComponent } from './app.component';
+
+/* Feature Modules */
+import { CoreModule } from './core/core.module';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'auth', loadChildren: 'app/authentication/authentication.module#AuthenticationModule'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      enableTracing: true
+    }),
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
